f0de326f359b3cb8030ff51a1ed7c374c66263cd
Daddodad <dav.moretti2@stud.uniroma3.it>
2024-08-02 16:53:41 +0200

    Added "Test Bond"

--- src/moves.py
+++ src/moves.py
@@ -284,6 +284,27 @@ On a *miss*, you are refused. _Pay the Price_.
                 """
             back_type = "back_to_relationship"
 
+        case "Test Bond":
+            text = """
+*TEST YOUR BOND*
+
+When your bond is tested through conflict, betrayal, or 
+circumstance, roll +heart.
+
+On a *strong hit*, this test has strengthened your bond. Choose one.
+• Take +1 spirit.
+• Take +2 momentum.
+
+On a *weak hit*, your bond is fragile and you must prove your loyalty. 
+Envision what they ask of you (_Ask the Oracle_ if unsure), and do it (or 
+_Swear an Iron Vow_). If you decline or fail, clear the bond and _Pay the 
+Price_.
+
+On a *miss*, or if you have no interest in maintaining this relationship, 
+clear the bond and _Pay the Price_.
+                """
+            back_type = 'back_to_relationship'
+
     keyboard = [
         [InlineKeyboardButton("Manual", callback_data=f"manual_{move_name}")],
         [InlineKeyboardButton("Back", callback_data=back_type)],
@@ -509,6 +530,14 @@ Also, respect the narrative weight of a bond. Don’t declare a bond with everyo
 If you and your allies act together to _Forge a Bond_ with an NPC or community, only one of you makes the move. Others can _Aid Your Ally_ to provide support. If you are successful, each of you may mark a tick on your bond progress track. Only the character making the move takes the mechanical benefits of a strong hit (+1 spirit or +2 momentum).
 
 Bonds can also be made between allies. One of you makes the move, and both of you may mark the bond on a hit. If you score a weak hit, your ally may decide what they ask of you. On a miss, something still stands between you. What is it? What must you do to form a deeper connection?
+"""
+        case "Test Bond":
+            text = """
+Bonds are not necessarily everlasting. Events in your story may cause your bond to be tested. How strong is your commitment? If you seek to maintain this bond, at what cost? When you are forced to act against a community or person you share a bond with, fail in a crucial task for them, or they break faith with you, make this move.
+
+You should _Test Your Bond_ within the community or in the company of the person with whom you share the bond. If an incident forces this test, but you aren’t in a position to resolve it, make a note. Then, make this move when you next come in contact. If extended time passes without making the test (days, weeks, or months, depending on the circumstance), clear the bond and be done with it.
+
+If you and your allies share a bond with an NPC or community, and you act together to _Test Your Bond_, only one of you makes this move.
 """
 
     if len(text) < 4096:
