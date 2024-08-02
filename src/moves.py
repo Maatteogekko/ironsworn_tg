7b5265876d7c72c777de87d92396b287ad576eba
Daddodad <dav.moretti2@stud.uniroma3.it>
2024-08-02 17:04:53 +0200

    added "Aid Ally"

--- src/moves.py
+++ src/moves.py
@@ -305,6 +305,15 @@ clear the bond and _Pay the Price_.
                 """
             back_type = 'back_to_relationship'
 
+        case "Aid Ally":
+            text = """
+*AID YOUR ALLY*
+
+When you _Secure an Advantage_ in direct support of an ally, and score a hit, they (instead of you) can take the benefits of the move. If you are in combat and score a strong hit, you and your ally have initiative.
+            """
+            back_type = 'back_to_relationship'
+
+
     keyboard = [
         [InlineKeyboardButton("Manual", callback_data=f"manual_{move_name}")],
         [InlineKeyboardButton("Back", callback_data=back_type)],
@@ -538,6 +547,22 @@ Bonds are not necessarily everlasting. Events in your story may cause your bond 
 You should _Test Your Bond_ within the community or in the company of the person with whom you share the bond. If an incident forces this test, but you aren’t in a position to resolve it, make a note. Then, make this move when you next come in contact. If extended time passes without making the test (days, weeks, or months, depending on the circumstance), clear the bond and be done with it.
 
 If you and your allies share a bond with an NPC or community, and you act together to _Test Your Bond_, only one of you makes this move.
+"""
+        case "Aid Ally":
+            text = """
+When you take an action to aid an ally (another player’s character) through the _Secure an Advantage_ move, you can hand over the benefits of that move to your ally. This represents setting your ally up for success through a supporting action. You might be distracting a foe in combat, scouting ahead on a journey, or giving them encouragement as you stand against a dire threat.
+
+If you score a strong hit when you _Secure an Advantage_, your ally makes the choice between +2 momentum or making an immediate move with a +1 add. If you have an asset which gives you any additional benefits on the outcome of a _Secure an Advantage_ move, your ally also takes those benefits (instead of you).
+
+In combat, this is a proactive move, made when you have initiative. If you score a strong hit, you and your ally both take or retain initiative.
+
+On a weak hit when you _Secure an Advantage_, your ally takes +1 momentum. But, this advantage is fleeting or your situation becomes more complicated or dangerous. If you are in combat, you both lose initiative.
+
+On a miss, one or both of you should _Pay the Price_ as appropriate to the circumstances and your intent when making the move. If in doubt, _Ask the Oracle_. As with a weak hit, you both lose initiative when in combat.
+
+If multiple characters make this move to contribute to an ally action, all _Secure an Advantage_ bonuses will stack. As long as someone scores a strong hit, the target character can take or retain initiative.
+
+Don’t ping pong this move back and forth between two characters in an attempt to build momentum. Envision what you are doing to _Aid Your Ally_, make the _Secure an Advantage_ move, resolve it, and hand the reins over to your ally as they leverage the advantage. Keep it moving. Make things happen.
 """
 
     if len(text) < 4096:
